dfb63fdf437bb31945c10e7c897adb276ef3f3a2
Van Duy <vanduy352003@gmail.com>
2023-12-16 16:28:37 +0700

    First commit

---
+++ cloudNhom6/src/main/java/hcmute/cloudNhom6/CloudNhom6Application.java
@@ -0,0 +1,13 @@
+package hcmute.cloudNhom6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNhom6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNhom6Application.class, args);
+	}
+
+}
